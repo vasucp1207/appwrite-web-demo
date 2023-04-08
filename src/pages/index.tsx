4e01486080481644d228190e9cde1faf484bff59
Vasu <vasucp1207@gmail.com>
2023-04-08 21:59:06 +0530

    env variable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
-import { SignInEmail, useConfig } from 'appwrite-web-react';
+import { SignInEmail } from 'appwrite-web-react';
 import "react-toastify/dist/ReactToastify.css";
 import { account } from '../../utils/utils';
 
 export default function Home() {
-  
   return (
     <div>
       <SignInEmail 
         account={account} 
         theme='light' 
         routePush='/room'
-        routeSign='https://amazing-cocada-e388a8.netlify.app/'
-        routeRst='https://amazing-cocada-e388a8.netlify.app/reset'
+        routeSign='http://localhost:3000/'
+        routeRst='http://localhost:3000/reset'
       />
     </div>
   )
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,8 +1,8 @@
 import { useConfig } from "appwrite-web-react";
 
 const { client, account, database, storage, avatars } = useConfig({
-  endpoint: 'http://localhost/v1',
-  projectId:'6351b10aa89690061fa4'
+  endpoint: process.env.REACT_APP_ENDPOINT,
+  projectId:process.env.REACT_APP_PROJECT
 });
 
 export {
